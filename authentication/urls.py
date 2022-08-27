edfbf7587b39c25719b8016cb2b0443bc9fa362a
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-08-27 18:44:09 +0530

    testing

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path, include
-from rest_framework.routers import DefaultRouter
-from .import views
-
-urlpatterns = [
-    path('', views.hello),
-    path('signup/', views.UserCreateView.as_view(), name='signup'),
-
-]
--- pizza/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Order
-# Register your models here.
-
-
-class OrderAdmin(admin.ModelAdmin):
-    list_display = [ 'order_status', 'quantity', 'created_at', 'updated_at']
-    list_filter = ['created_at', 'order_status', 'quantity']
-admin.site.register(Order, OrderAdmin)
